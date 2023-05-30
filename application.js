d4e995c1a6702b5a007841671135b0eeaa9e1c3d
RohtashNegi <129067050+RohtashNegi@users.noreply.github.com>
2023-05-30 20:16:39 +0530

    removed old comment

--- application.js
+++ application.js
@@ -3,7 +3,7 @@ var firstButton = document.getElementsByTagName("button")[0];
 var incompleteTaskHolder = document.getElementById("incomplete-tasks");
 var completedTasksHolder = document.getElementById("completed-tasks");
 
-//removing old comment
+
 var CreatingNewElements = function (taskString) {
   var newlist = document.createElement("li");
   var checkBox = document.createElement("input");
