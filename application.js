95bf5b65d702859d06e624a3a2971e393e1a94da
RohtashNegi <129067050+RohtashNegi@users.noreply.github.com>
2023-06-02 02:30:53 +0530

    Update application.js

--- application.js
+++ application.js
@@ -3,7 +3,7 @@ var firstButton = document.getElementsByTagName("button")[0];
 var incompleteTaskHolder = document.getElementById("incomplete-tasks");
 var completedTasksHolder = document.getElementById("completed-tasks");
 
-//new
+//new one changes in this file
 
 var CreatingNewElements = function (taskString) {
   var newlist = document.createElement("li");
