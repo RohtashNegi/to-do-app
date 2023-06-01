5f48430737fbecdc86ee94eb8a1b9ec74dddecbb
Rohtash Negi <rohtashnegi1998@gmail.com>
2023-06-02 00:47:22 +0530

    learning git

--- application.js
+++ application.js
@@ -38,15 +38,14 @@ var addTask = function () {
     var newlist = CreatingNewElements(taskInput.value);
 
     incompleteTaskHolder.appendChild(newlist);
-    // bindTaskEvents(newlist, taskCompleted);  // use of bindTaskEvents
+    bindTaskEvents(newlist, taskCompleted);
   }
   taskInput.value = "";
 };
-firstButton.addEventListener("click", addTask);
+// firstButton.addEventListener("click", addTask); // to add new list in UL
 
 var editTask = function () {
-  var newlist = this.parentElement; // this keyword refering to ?
-
+  var newlist = this.parentNode; // this keyword refering to ?
   var editInput = newlist.querySelector("input[type=text]");
   var newlabel = newlist.querySelector("label");
   var containsClass = newlist.classList.contains("editMode");
@@ -56,7 +55,63 @@ var editTask = function () {
     editInput.value = newlabel.innerText;
   }
   newlist.classList.toggle("editMode");
-  //   incompleteTaskHolder.appendChild(newlist);
 };
-var editButton = document.querySelector("button.edit");
-editButton.onclick = editTask;
+
+// Delete Task
+
+var deleteTask = function () {
+  var newlist = this.parentNode;
+  // console.log(newlist);
+  var ul = newlist.parentNode;
+  // console.log(ul);
+  ul.removeChild(newlist);
+};
+
+// document.querySelector(".delete").addEventListener("click", deleteTask);
+
+//Mark task completed
+var taskCompleted = function () {
+  //Append the task list item to the #completed-tasks
+  var listItem = this.parentNode;
+  completedTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskIncomplete);
+};
+
+var taskIncomplete = function () {
+  var listItem = this.parentNode;
+  incompleteTaskHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+};
+
+// var ajaxRequest = function () {    why created this function if no use of this function
+//   console.log("AJAX Request");
+// };
+
+firstButton.addEventListener("click", addTask);
+// firstButton.addEventListener("click", ajaxRequest);
+
+var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
+  console.log("bind list item events");
+  //select ListItems children
+  var checkBox = taskListItem.querySelector("input[type=checkbox]");
+  var editButton = taskListItem.querySelector("button.edit");
+  var deleteButton = taskListItem.querySelector("button.delete");
+
+  //Bind editTask to edit button.
+  editButton.onclick = editTask;
+  //Bind deleteTask to delete button.
+  deleteButton.onclick = deleteTask;
+  //Bind taskCompleted to checkBoxEventHandler.
+  checkBox.onchange = checkBoxEventHandler;
+};
+
+for (var i = 0; i < incompleteTaskHolder.children.length; i++) {
+  //bind events to list items chldren(tasksCompleted)
+  bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
+}
+
+//cycle over completedTasksHolder ul list items
+for (var i = 0; i < completedTasksHolder.children.length; i++) {
+  //bind events to list items chldren(tasksIncompleted)
+  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+}
